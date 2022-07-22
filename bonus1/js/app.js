6df11b6878aa47b0a12c9737a41c5ea90a07371c
Bilobra <bra.carrozzo@gmail.com>
2022-07-22 17:55:26 +0200

    add style grid

--- bonus1/js/app.js
+++ bonus1/js/app.js
@@ -11,20 +11,34 @@ for (let i = 0; i < 100; i++) {
     const number = i + 1
     // console.log(number)
 
-    //  creaimo un elemento
+    // //  creaimo un elemento
     const divElement = document.createElement('div')
     // lo modifichiamo mettendogli qualcosa all'interno
+    divElement.classList.add('cell')
     divElement.append(number)
     console.log(divElement)
 
-    if (number % 3 == 0 && number % 5 == 0) {
-        console.log( 'BuzzFizz') // per i numeri sia multipli di 3 che di 5 stami fizzbuzz
-    } else if (number % 5 == 0) {
-        console.log('Buzz') // per i multipli di 5 stampi buzz
+    
 
+    if (number % 3 == 0 && number % 5 == 0) {
+        // console.log( 'BuzzFizz') // per i numeri sia multipli di 3 che di 5 stami fizzbuzz
+        divElement.append(`BuzzFizz`)
+        divElement.classList.add('buzzfizz_cell')
+    } else if (number % 5 == 0) {
+        // console.log('Buzz') // per i multipli di 5 stampi buzz
+        divElement.append(`Buzz`)
+        divElement.classList.add('buzz_cell')
     } else if (number % 3 == 0) {
-        console.log( 'Fizz') // per i multipli di 3 stampi fizz al posto dei numeri
+        // console.log( 'Fizz') // per i multipli di 3 stampi fizz al posto dei numeri
+        divElement.append(`Fizz`)
+        divElement.classList.add('fizz_cell')
     }
+    else {
+        // console.log (number) // per i numeri che non rientrano nella condizione stamp i + 1
+        divElement.classList.add('normal_cell')
+    }
+
+    gridElement.append(divElement)
 
 
 
