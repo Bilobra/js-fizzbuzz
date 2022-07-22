2df20b50f1904e2a53b06d9d24fb62fe1b691b10
Bilobra <bra.carrozzo@gmail.com>
2022-07-22 16:09:09 +0200

    for + %3 % 5

--- js/app.js
+++ js/app.js
@@ -1,2 +1,24 @@
 // console log di prova
 console.log('hello!')
+
+// scrivi programma che stampi in console n° da 1 a 100
+for (let i = 0; i < 100; i++) {
+    const number = i + 1
+    console.log(number)
+
+    
+    if ( number % 5) {
+        console.log( number + 'Buzz')
+    } else (number % 3) {
+        console.log( number + 'fizz')
+    } 
+    
+    // per i multipli di 3 stampi fizz al posto dei numeri
+
+    // per i multipli di 5 stampi buzz
+
+
+    // per i numeri sia multipli di 3 che di 5 stami fizzbuzz
+
+
+}
